7d412cc53880e50851076cec8cfc504c25acd4ab
Michael <michaelcclary@gmail.com>
2021-10-29 09:38:24 -0500

    Last entry for redux

--- Frontend/src/parkRoutes/Activities.test.js
+++ Frontend/src/parkRoutes/Activities.test.js
@@ -1,19 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import 'regenerator-runtime/runtime'
-import Homepage from './Homepage';
+import Activities from './Activities';
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../store"
 
 
-const initialState = {
-    users: null,
-};
-
 //smoke test
 it('it renders without crashing', () => {
     <Provider store={store}>
-        render(<Homepage />)
+        render(<Activities />)
     </Provider>
 })
-
